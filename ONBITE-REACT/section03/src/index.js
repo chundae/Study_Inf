db90102469436d2e382a96205c04a23eb5c25f46
kiseok <kisunk4546@gmail.com>
2024-10-31 19:02:00 +0900

    Node.js Basics

---
+++ ONBITE-REACT/section03/src/index.js
@@ -0,0 +1,16 @@
+import { add, sub } from "./math.js";
+import mul from "./math.js";
+
+// module system
+// const moduleData = require("./math");
+// console.log(moduleData);
+
+// console.log(moduleData.add(1, 2));
+// console.log(moduleData.sub(1, 2));
+
+// //repacktoring
+// const { add, sub } = require("./math");
+
+console.log(add(1, 2));
+console.log(sub(1, 2));
+console.log(mul(2, 3));
---
+++ ONBITE-REACT/section03/src/randomIndex.js
@@ -0,0 +1,5 @@
+import randomColor from "randomcolor";
+
+var color = randomColor();
+
+console.log(color);
