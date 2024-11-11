20afd62473232687e680b3e82f2073def46485dc
kiseok <kisunk4546@gmail.com>
2024-11-11 21:23:48 +0900

    project Main(Home.jsx) service Create

--- ONBITE-REACT/section07/src/components/DiaryItem.jsx
+++ ONBITE-REACT/section07/src/components/DiaryItem.jsx
@@ -1,26 +1,31 @@
 import './DiaryItem.css';
 import {getEmotionImage} from './../util/get-emotion-image.js';
 import Button from "./Button.jsx";
+import {useNavigate} from "react-router-dom";
 
-const DiaryItem = () => {
+const DiaryItem = ({id, createdDate, emotionId, content }) => {
 
-    const emotionId = 1;
+    const nav = useNavigate();
 
     return (
         <div className="DiaryItem">
-            <div className={`img_section img_section_${emotionId}`}>
+            <div onClick={() => nav(`/diary/${id}`)}
+                 className={`img_section img_section_${emotionId}`}>
                 <img src={getEmotionImage(emotionId)} alt=""/>
             </div>
-            <div className="info_section">
+            <div onClick={() => nav(`/diary/${id}`)}
+                className="info_section">
                 <div className="created_date">
-                    {new Date().toLocaleDateString()}
+                    {new Date(createdDate).toLocaleDateString()}
                 </div>
                 <div className="content">
-                    일기
+                    {content}
                 </div>
             </div>
             <div className="button_section">
-                <Button text={"수정하기"}/>
+                <Button
+                    onClick={() => nav(`/edit/${id}`)}
+                    text={"수정하기"}/>
             </div>
         </div>
     );
--- ONBITE-REACT/section07/src/pages/Home.jsx
+++ ONBITE-REACT/section07/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Header from "../components/Header.jsx";
 import Button from "../components/Button.jsx";
 import DiaryList from "../components/DiaryList.jsx";
 import {useState, useContext} from "react";
-import {DiaryStateContext} from "../App.jsx";
+import {DiaryStateContext, DiaryDispatchContext} from "../App.jsx";
 
 const getMonthlyData = (pivotDate, data) => {
     //pivot 을 기준으로 달의 시작
@@ -18,18 +18,25 @@ const getMonthlyData = (pivotDate, data) => {
         pivotDate.getMonth() + 1,
         0, 23, 59, 59).getTime();
 
-    return data.filter((item) => beginTime <= item.createdDate <= endTime);
+    // filter 를 사용해 해당 달에 있는 DiaryList 필터링
+    return data.filter((item) => {
+        beginTime <= item.createdDate && item.createdDate <= endTime
+    });
 }
 
-const Home = () => {
-    const data = useContext(DiaryStateContext);
 
+const Home = () => {
+    //App.jsx에서 DiaryItem에 대한 데이터를 Context형태로 사용하기 위함
+    const data = useContext(DiaryStateContext);
     const [pivotDate, setPivotDate] = useState(new Date());
 
+    const monthlyData = getMonthlyData(pivotDate, data);
+
+    //이번달 기준 + 1
     const onIncreaseMonth = () => {
         setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
     };
-
+    //이번달 기준 - 1
     const onDecreaseMonth = () => {
         setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
     };
@@ -37,10 +44,12 @@ const Home = () => {
     return (
         <>
             <Header title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`}
+                //title -> Button 파라미터로 Button에서 동작할 콜백함수를 전송
                     leftChild={<Button text={"<"} onClick={onDecreaseMonth}/>}
                     rightChild={<Button text={">"} onClick={onIncreaseMonth}/>}
             />
-            <DiaryList/>
+            <DiaryList data={monthlyData}/>
+
         </>
     );
 }
